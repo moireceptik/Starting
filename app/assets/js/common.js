6fedbd5a081ef0bd5ecccafaf9f3a302b1adf460
moireceptik <moireceptik@yandex.ru>
2020-09-07 15:06:03 +0700

    update

--- app/assets/js/common.js
+++ app/assets/js/common.js
@@ -1,5 +1,20 @@
 "use strict";
 
+/* ==========================================================================
+   DM Top
+========================================================================== */
+$(window).scroll(function(){
+	if ($(this).scrollTop() > 1) {
+		$('.dmtop').css({bottom:"25px"});
+	} else {
+		$('.dmtop').css({bottom:"-100px"});
+	}
+});
+$('.dmtop').click(function(){
+	$('html, body').animate({scrollTop: '0px'}, 800);
+	return false;
+});
+
 $(document).ready(function() {
 
 }); //end $(document).ready(function(
